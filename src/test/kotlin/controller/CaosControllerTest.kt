c47f78a08de550799453f6833f81b06ad55adae0
Kieran Murphy <kimurphy@thoughtworks.com>
2021-03-04 17:46:17 -0600

    adds CaosControllerTest

---
+++ src/test/kotlin/controller/CaosControllerTest.kt
@@ -0,0 +1,27 @@
+package com.caos.api.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(CaosController::class)
+internal class CaosControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `canary test`() {
+        assert(true)
+    }
+
+    @Test
+    fun `return hello world`() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk)
+    }
+
+}
